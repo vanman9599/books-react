580ad6e15b2ddae7c002843fce18e06cfe6a3822
unknown <vanjordan46@hotmail.com>
2024-02-14 19:08:08 -0800

    almost done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,51 @@
+import {useState, useEffect} from 'react'
+import BookCreate from './components/BookCreate'
+import BookList from './components/BookList';
+import axios from 'axios';
+
 function App(){
-    return <div>App</div>
+    const [books, setBooks] = useState([]);
+    useEffect(()=>{
+        fetchBooks();
+    },[])
+    const fetchBooks = async() =>{
+        const response = await axios.get('http://localhost:3001/books');
+        setBooks(response.data)
+    }
+    const createBook = async(title)=>{
+       const response = await axios.post('http://localhost:3001/books', {
+          title   
+        });
+        setBooks([...books, response.data])
+        
+    }
+    
+
+    const deleteBookById = (id) => {
+        const updatedBooks = books.filter((book)=>{
+            return book.id !== id;
+        })
+
+        setBooks(updatedBooks)
+    }
+
+    const editBookById = (id,newTitle)=>{
+        const updatedBooks = books.map((book)=>{
+            if(book.id ===id){
+                return {...book, title: newTitle}
+            }
+            return book;
+        });
+        setBooks(updatedBooks)
+    }
+    
+    return (
+    <div className="app">
+        <h1>Reading List</h1>
+        <BookList onEdit={editBookById} onDelete={deleteBookById}  books={books}/>
+        <BookCreate onCreate={createBook} />
+        </div>
+        )
 }
 
 export default App;
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,5 +1,10 @@
-function BookList() {
-    return <div>Book List</div>
+import BookShow from "./BookShow";
+
+function BookList({books, onDelete, onEdit}) {
+ const renderedBooks = books.map((book)=>{
+    return <BookShow onEdit={onEdit} onDelete={onDelete} book={book} key={book.id} />
+ }) 
+ return <div className="book-list">{renderedBooks}</div>
 }
 
 export default BookList
